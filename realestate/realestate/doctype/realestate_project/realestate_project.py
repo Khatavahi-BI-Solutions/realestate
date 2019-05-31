e56459b9d0d13bd7a9557e02040c901e55735217
Jigar Tarpara <jigartarpara68@gmail.com>
2019-05-31 12:02:24 +0530

    Quick fix

--- realestate/realestate/doctype/realestate_project/realestate_project.py
+++ realestate/realestate/doctype/realestate_project/realestate_project.py
@@ -7,44 +7,29 @@ import frappe
 from frappe.model.document import Document
 
 class RealEstateProject(Document):
-	def validate(self):
-		realestate_settings = frappe.get_doc("RealEstate Settings", "RealEstate Settings")
-		if not self.item:
-			self.create_item()
-		else:
-			item = frappe.get_doc("Item",self.item)
-			self.update_item_description(item)
-		# self.create_update_item_price(realestate_settings)
+	def after_insert(self):
+		if not self.project:
+			self.create_project(True)
 	
-	def create_item(self):
-		if frappe.db.get_value("Item", self.assets_name, "name"):
-			item = frappe.get_doc("Item",self.assets_name)
-		else:
-			item = frappe.get_doc({
-				"doctype": "Item",
-				"item_code": self.assets_name,
-				"item_group": self.item_group,
-				"description": self.assets_details
-			})
-			item.save()
-		self.item = item.name
-	
-	def create_update_item_price(self, realestate_settings):
-		if frappe.db.get_values("Item Price",filters={"item_code":self.item, "price_list":realestate_settings.price_list}, fieldname=["name"]):
-			
-			item_price = frappe.get_doc("Item Price",frappe.db.get_values("Item Price",filters={"item_code":self.item, "price_list":realestate_settings.price_list}, fieldname=["name"]))
-			item_price.price_list_rate = self.price_list_rate
-			item_price.save()
-		else:
-			
-			item_price = frappe.get_doc({
-				"doctype": "Item Price",
-				"item_code": self.item,	
-				"price_list": realestate_settings.price_list,
-				"price_list_rate": self.price_list_rate
-			})
-			item_price.save()
+	def before_remove(self):
+		if self.project:
+			self.project=""
+			self.save()
+			return "Project Unlinked"
 
-	def update_item_description(self, item):
-		item.description = self.assets_details
-		item.save()
+	def create_project(self, from_after_insert = False):
+		if frappe.db.get_value("Project", self.project_name, "name"):
+			project = frappe.get_doc("Project",self.project_name)
+		else:
+			project = frappe.get_doc({
+				"doctype": "Project",
+				"project_name": self.project_name,
+				"real_estate_project": self.name
+			})
+			project.save()
+		self.project = project.name
+		if not from_after_insert:
+			self.save()
+	
+	def delete_project(self, project):
+		frappe.delete_doc("Project", project)
